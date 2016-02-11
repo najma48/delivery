992132f31b2f2ad4aabeae263e32fa6b4851152a
Zahra <the-peace15@gmail.com>
2016-02-11 22:38:37 +0300

    first

---
+++ src/delivery/Delivery.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delivery;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author zahra
+ */
+public class Delivery {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+       
+        
+        BufferedReader br = null;
+        String currentLine;
+        br = new BufferedReader(new FileReader("busy_day.in"));
+        int R,C,D,T, maxLoad;
+        currentLine = br.readLine();
+        String Fline[] = new String[5];
+        Fline = currentLine.split(" ");
+        R= Integer.parseInt(Fline[0]);
+        C= Integer.parseInt(Fline[1]);
+        D= Integer.parseInt(Fline[2]);
+        T= Integer.parseInt(Fline[3]);
+        maxLoad= Integer.parseInt(Fline[4]);
+        System.out.print("done");
+    }
+    
+}
